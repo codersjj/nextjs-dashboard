cbde47bf0c01d5b9c0a6b7a51c9f2e319207dfb3
codersjj <shanewestlife@outlook.com>
2025-05-05 22:03:24 +0800

    feat(Chapter15): Protecting your routes with Next.js Middleware

--- auth.config.ts
+++ auth.config.ts
@@ -4,4 +4,18 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoginIn = !!auth?.user
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
+      if (isOnDashboard) {
+        if (isLoginIn) return true
+        return false // Redirect unauthenticated users to login page
+      } else if (isLoginIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl))
+      }
+      return true
+    }
+  },
+  providers: [] // Add providers with an empty array for now
 } satisfies NextAuthConfig;
